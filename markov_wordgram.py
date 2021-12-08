f296d225c301c783af64435b408915aa5ffbbeed
ezeocm <ezeocm7471@wwcsd.org>
2021-12-08 08:01:36 -0500

    Create markov_wordgram.py

---
+++ markov_wordgram.py
@@ -0,0 +1,28 @@
+import random
+path='data.txt'
+with open(path) as f:
+    wordList = f.read().lower().split()
+    f.close()
+ngrams = {}
+#the main algorithm that will run the game
+def Markov(sample, length):
+    for i in enumerate(sample):
+        try:
+            ngrams[i[1]].append(sample[i[0]+1])
+        except KeyError:
+            ngrams[i[1]] = []
+            try:
+                ngrams[i[1]].append(sample[i[0]+1])
+            except IndexError:
+                continue
+    currentgram = random.choice(sample)
+    result = currentgram
+    for i in range(length):
+        poss = ngrams[currentgram]
+        nexT = random.choice(poss)
+        result += ' '+nexT
+        currentgram = random.choice(ngrams[result.split()[-1]])
+    return result
+
+if __name__ == '__main__':
+    print(Markov(wordList,random.choice(range(10,50))))
