31369239ebf81439b5b98dfd84de4c6e3bf7496a
ezeocm <91962031+ezeocm@users.noreply.github.com>
2021-12-09 08:47:28 -0500

    Update discord_scraper.py

--- discord_scraper.py
+++ discord_scraper.py
@@ -1,2 +1,18 @@
 import requests
 import json
+
+def retrieveMessage(channel):
+    headers = {
+        'authorization': 'NTQ5NjgwNzY0MjM3Nzc0ODUw.YYTFGg.uqUElYolGooNaZPKW9TGzCRqGlg'
+        
+        }
+    url = f'https://discord.com/api/v9/channels/{channel}/messages?limit=101'
+    r=requests.get(url, headers=headers)
+    jsonn = json.loads(r.text)
+    for value in jsonn:
+        try:
+            if value['author']['username'] == 'BeanZ':
+                print('Chuka:',value['content'])
+        except TypeError:
+            print(value)
+retrieveMessage('627538418943000597')
