2ed4e4ce2b1adefa1f74247b3a88bc8bd8b8cfc5
ezeocm <91962031+ezeocm@users.noreply.github.com>
2021-12-31 03:04:34 -0500

    scraped discord messages using py file

--- discordScraped/discord_scraper.py
+++ discordScraped/discord_scraper.py
@@ -1,26 +1,60 @@
-'''
-import requests
-import json
-
-def retrieveMessage(channel):
-    headers = {
-        'authorization': 'NTQ5NjgwNzY0MjM3Nzc0ODUw.YYTFGg.uqUElYolGooNaZPKW9TGzCRqGlg'
-        
-        }
-    url = f'https://discord.com/api/v9/channels/{channel}/messages?limit=101'
-    r=requests.get(url, headers=headers)
-    jsonn = json.loads(r.text)
-    for value in jsonn:
-        try:
-            if value['author']['username'] == 'BeanZ':
-                print('Chuka:',value['content'])
-        except TypeError:
-            print(value)
-retrieveMessage('627538418943000597')
-This is a good method for fetching recent messages, not good for my purposes
-'''
-import pyautogui as pag
+import mouse as m
+from time import sleep as s
+import keyboard
+import clipboard
+textPath = 'data.txt'
+beanPath = 'BeanZ.txt'
 def capture():
-    '''
-    see BeanZ's messages then go next'''
     
+    """
+    scrape BeanZ's messages then go next
+    """
+    topPageLeftCorner = (882,179)#(1329,266)
+    dragAt = (1150, 719)
+    nextButton = (1209, 648)
+    m.move(topPageLeftCorner[0], topPageLeftCorner[1])
+    s(1)
+    m.press()
+    s(.5)
+    m.move(dragAt[0],dragAt[1],duration=1.2)
+    s(2.5)
+    m.move(nextButton[0],nextButton[1],duration=.5)
+    m.release()
+    s(.5)
+    keyboard.press_and_release('ctrl+c')
+    s(.01)
+    data=clipboard.paste()
+    m.click()
+    s(3)
+    with open(textPath,'wb') as f:
+        f.write(data.encode())
+        f.close()
+def clean():
+    user = b'WINGU INTJ Sigma Lifter yb stan'
+    tempList =[]
+    index = 0
+    endIndex=0
+    wrongIndices=[]
+    with open(textPath,'rb') as f:
+        textList = f.readlines()
+        for line in textList:
+            if user in line or b'GIF' in line or b'Image' in line:
+                continue
+            elif b'//twitter.com/' in line:
+                endIndex = index+10
+            elif b'parsec.gg/' in line:
+                endIndex = index+4
+                pass
+            if index > endIndex:
+                tempList.append(line)
+            
+            index+=1
+        f.close()
+    tempList=list(map(lambda x: x.replace(b'\r\n',b'')+b'\n',tempList))
+    with open(beanPath,'ab') as q:
+        q.writelines(tempList)
+        q.close()
+for i in range(26):
+    capture()
+    clean()
+
